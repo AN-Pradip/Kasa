b0b9326817f2810a84ed3b00b4ccedc142198bb7
AN-Pradip <ppradip.an@gmail.com>
2022-12-17 15:14:39 +0100

    Creating Homepage articles desktop
    Adding js ,scss, img for the articles for the Homepage.
    Deleting some files

---
+++ src/components/LodgingItems.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import lodgings from "../LodgingData.json";
+
+function LodgingItems(){
+    return(
+        <section className="LodgingItems--Container">
+            {lodgings.map((lodging) => {
+                return(
+                    <article key={lodging.id}>
+                        <Link to={`/lodging/${lodging.id}`} style={{ textDecoration: 'none' }}>
+                            <div className="LodgingItems--articles">
+                                <img src={lodging.cover} alt="" className="articles--img" />
+                                <h2 className="articles--title">{lodging.title}</h2>
+                            </div>
+                        </Link>
+                    </article>
+                )
+                })}
+        </section>
+    )
+}
+
+export default LodgingItems;
+//<div className="article--overlay"></div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import HomepageBanner from "../components/HomepageBanner";
+import LodgingItems from "../components/LodgingItems";
 
 function Homepage(){
     return(
         <div className="Homepage">
             <HomepageBanner />
+            <LodgingItems />
         </div>
     )
 }
