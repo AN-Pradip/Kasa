78a3432204cd5190524d7ed6eacd10d922f74c48
AN-Pradip <ppradip.an@gmail.com>
2022-12-17 12:23:34 +0100

    Creation du Header desktop
    Ajout du Header avec les routes, les liens, image et scss

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+import logo from "../assets/LOGO.png";
+
+
+function Header(){
+    return(
+        <header>
+            <Link to="/">
+                <img src={logo} alt="logo"/>
+            </Link>
+            <nav>
+                <NavLink to="/" className="nav--Links">
+                    Accueil
+                </NavLink>
+                <NavLink to="/" className="nav--Links">
+                    About
+                </NavLink>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
